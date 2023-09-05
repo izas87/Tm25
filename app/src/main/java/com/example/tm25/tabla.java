53491e54b0667663d39d93c0e315eccc088da507
izas87 <140190888+izas87@users.noreply.github.com>
2023-09-05 17:39:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tm25/tabla.java
@@ -0,0 +1,14 @@
+package com.example.tm25;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tabla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabla);
+    }
+}
